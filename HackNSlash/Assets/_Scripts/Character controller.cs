44e579a9f49c9328257545d66899bb4900c18188
Adrian Cummings <cummings,adrian@gmail.com>
2018-02-10 18:09:04 -0500

    First commit
    Created placeholder person and terrain.

---
+++ HackNSlash/Assets/_Scripts/Character controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charactercontroller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+
+    //DFGHH
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
